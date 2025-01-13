0a29bb3bb436fa814b42fb45b92c99c0fc94b769
sivakumarmanoharan <siva.ms2000@gmail.com>
2025-01-13 09:34:28 -0500

    Projects section completed

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -12,17 +12,10 @@ import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import heroImage from '../images/pexels-raulling-30030950.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
 import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic2 from '../images/20241224_114854.jpg'
 import testimonialImage from '../images/testimonial.webp';
 import shreyasImage from '../images/1516824683222.webp'
@@ -253,71 +246,30 @@ export const skills: SkillGroup[] = [
  * Portfolio section
  */
 export const portfolioItems: PortfolioItem[] = [
+ 
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'ReUnite',
+    description: 'A comprehensive project that helps university students to buy/sell reusable products at a lower cost. The project is developed using Django, HTML, CSS and Bootstrap.',
+    url: 'https://github.com/sivakumarmanoharan/reunite_group_26',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Agrosphere',
+    description: 'A comprehensive platform which helps small scale farmers to achieve maximum production of crops.',
+    url: 'https://github.com/sivakumarmanoharan/Agrosphere',
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Password Generator',
+    description: 'A simple password generator that generates a password based on the length provided',
+    url: 'https://github.com/sivakumarmanoharan/password-generator-flask',
     image: porfolioImage7,
   },
   {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'CropSuitability',
+    description: `This initiative aims to empower farmers, entrepreneurs, and investors with data-driven insights for strategic decision-making in agriculture. By analyzing historical crop performance, it seeks to foster sustainable practices, enhance resilience to climate change, and promote prosperity in Canada's rural communities.`,
+    url: 'https://github.com/sivakumarmanoharan/A-Data-informed-Evalution-for-crop-suitability-using-Data-Analystics-and-ML',
     image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
   },
 ];
 
